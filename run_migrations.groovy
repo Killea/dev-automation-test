3b18311db21b8fc6cf26137ec2809d9f6da7aff9
Hanking <killea@gmail.com>
2019-10-14 09:54:57 -0600

    fix run_migrations.groovy

--- run_migrations.groovy
+++ run_migrations.groovy
@@ -3,10 +3,11 @@ pipeline {
     stages {
         stage('Database init') {
             steps {
-                cmd = """#!/bin/sh -e\n' + 'python3 run_migrations.py  ${TENANT_NAMES} ${MIGRATION_NAME}"""
-                sh(script: cmd)
+               script { cmd = """python3 /home/run_migrations.py  ${TENANT_NAMES} ${MIGRATION_NAME} ${RUN_TYPE}"""
+                result = sh(returnStdout:true , script: cmd).trim()
+                echo "Python output: ${result}"     
+            }
             }
     }
     }
 }
-
